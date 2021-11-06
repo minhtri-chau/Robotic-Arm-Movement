fb26257385d12c8cc564400ac564ac8bd7577496
Minh Tri Chau <chauminhtri156@gmail.com>
2021-11-06 03:43:35 -0700

    adding simultaneous functions fix + changing the parameter for read and write data function
    instead of having index of the DXL_ID array as the input, now the read and write data functions directly take in the ID of the motor as the input

--- Motor Driver/motorctrl_v1.py
+++ Motor Driver/motorctrl_v1.py
@@ -99,9 +99,7 @@ def dxlPresAngle(dxlIDs):
 
     print("DXL IDs being read: ", dxlIDs)
     for id in range(idNum): #Reads the current position of the motor
-        for motorIndex in range(motorNum):
-            if (DXL_ID[motorIndex] == dxlIDs[id]):
-                dxl_present_position[id] = ReadMotorData(motorIndex, ADDR_PRESENT_POSITION)
+        dxl_present_position[id] = ReadMotorData(dxlIDs[id], ADDR_PRESENT_POSITION)
     print("Present positions are: ", dxl_present_position)
 
     for id in range(idNum): #Converts the position into angles
@@ -115,9 +113,7 @@ def dxlSetVelo(vel_array, dxlIDs):
     if (len(vel_array) == len(dxlIDs)):
         idNum = len(dxlIDs)
         for id in range(idNum):
-            for motorIndex in range(motorNum):
-                if (DXL_ID[motorIndex] == dxlIDs[id]): 
-                    WriteMotorData(motorIndex, ADDR_PROFILE_VELOCITY, vel_array[id])
+                    WriteMotorData(dxlIDs[id], ADDR_PROFILE_VELOCITY, vel_array[id])
     else:
         print("ERROR: Number of velocity inputs not matching with number of DXL ID inputs!")
     print("-------------------------------------")
@@ -130,9 +126,7 @@ def dxlGetVelo(dxlIDs):
 
     print("DXL IDs being read: ", dxlIDs)
     for id in range(idNum):
-            for motorIndex in range(motorNum):
-                if (DXL_ID[motorIndex] == dxlIDs[id]): 
-                    dxl_present_velocity[id] = ReadMotorData(motorIndex, ADDR_PROFILE_VELOCITY)
+        dxl_present_velocity[id] = ReadMotorData(dxlIDs[id], ADDR_PROFILE_VELOCITY)
     print("Velocities are ", dxl_present_velocity)
     print("-------------------------------------")
     return (dxl_present_velocity)
@@ -154,21 +148,16 @@ def motorRunWithInputs(angle_inputs, dxlIDs):
         while 1:
             #Convert angle inputs into step units for movement
             for id in range(idNum):
-                for motorIndex in range(motorNum):
-                    if (DXL_ID[motorIndex] == dxlIDs[id]):  
-                        dxl_goal_inputs[id] = _map(
-                    dxl_goal_angle[id], 0, 360, 0, 4095)
+                dxl_goal_inputs[id] = _map(dxl_goal_angle[id], 0, 360, 0, 4095)
             print("Goal angles are ", dxl_goal_angle)
 
             #Write goal position for all motors (base, bicep, forearm, wrist, claw)
             for id in range(idNum):
-                for motorIndex in range(motorNum):
-                    if (DXL_ID[motorIndex] == dxlIDs[id]):
-                        WriteMotorData(motorIndex, ADDR_GOAL_POSITION, dxl_goal_inputs[id])
+                WriteMotorData(dxlIDs[id], ADDR_GOAL_POSITION, dxl_goal_inputs[id])
 
-                        #Read position for each motor and set status of motor
-                        dxl_end_position[id], movementStatus[id] = motor_check(motorIndex, dxl_goal_inputs[id]) #Read position for the motor
-                        dxl_end_angle[id] = _map(dxl_end_position[id], 0, 4095, 0, 360)
+                #Read position for each motor and set status of motor
+                dxl_end_position[id], movementStatus[id] = motor_check(dxlIDs[id], dxl_goal_inputs[id]) #Read position for the motor
+                dxl_end_angle[id] = _map(dxl_end_position[id], 0, 4095, 0, 360)
             #print("Angle for Dynamixel:%03d is %03d" % (DXL_ID[device_index], dxl_end_angle[device_index]))
 
             for id in range(idNum):
@@ -182,69 +171,41 @@ def motorRunWithInputs(angle_inputs, dxlIDs):
     else:
         print("ERROR: Number of angle inputs not matching with number of DXL ID inputs")
 
-#The functions take in an array of X angle inputs and an array of X dynamixel IDs. Running the functions will drive the X dynamixels to the desired angle inputs simultaneously
-def Sim2MotorsRun(angle_inputs, dxlIDs):
-    #Intializate simultaneous motor movement
-    global motor_sync_write
-    motor_sync_write = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_POSITION, LEN_GOAL_POSITION)
-    global motor_sync_read
-    motor_sync_read = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
-    #print("Motor_Sync_Write is ", motor_sync_write)
-    #print("Motor_Sync_Read is ", motor_sync_read)
-    dxl_goal_angle = angle_inputs
-    dxl_goal_inputs = [0] * len(angle_inputs)
-    param_goal_position = [0] * len(angle_inputs)
 
-    #Convert angle inputs into step units for movement
-    for motorIndex in range(len(angle_inputs)): 
-        dxl_goal_inputs[motorIndex] = _map(dxl_goal_angle[motorIndex], 0, 360, 0, 4095)
-        print("Goal angles are ", dxl_goal_angle)
+def simMotorRun(angle_inputs, dxlIDs):
+    idNum = len(dxlIDs)
+    movementStatus = [1] * idNum
 
-    #Create parameter storage for present positions
-    for motorID in dxlIDs:  #Create parameter storage for bicep and forearm motors' present position value
-        dxl_addparam_result = motor_sync_read.addParam(motorID)
-        if dxl_addparam_result != True:
-            print("[ID:%03d] groupSyncRead addparam failed" % motorID)
-        #print("DXL_ADDPARAM_RESULT is " ,dxl_addparam_result)
+    #Format is [base, bicep, forearm, wrist, claw]
+    if (len(angle_inputs) == idNum):
+        dxl_goal_angle = angle_inputs
+        dxl_goal_inputs = [0] * idNum
+        dxl_end_position = [0] * idNum
+        dxl_end_angle = [0] * idNum
+        movementStatus = [0] * idNum
 
-    #Allocate goal position values into 4-byte array for bicep and forearm motors. Dynamixel motors use either 2-bytes or 4-bytes
-    for motorIndex in range(len(angle_inputs)): 
-        param_goal_position[motorIndex] = [DXL_LOBYTE(DXL_LOWORD(dxl_goal_inputs[motorIndex])), DXL_HIBYTE(DXL_LOWORD(dxl_goal_inputs[motorIndex])),DXL_LOBYTE(DXL_HIWORD(dxl_goal_inputs[motorIndex])), DXL_HIBYTE(DXL_HIWORD(dxl_goal_inputs[motorIndex]))]
-    #print("param_goal_bicep_position is ", param_goal_position[1])
-    #print("param_goal_bicep_position is ", param_goal_position[2])
-        #Add goal position input values of bicep and forearm motors to Syncwrite parameter storage
-        dxl_addparam_result = motor_sync_write.addParam(dxlIDs[motorIndex], param_goal_position[motorIndex])
-        if dxl_addparam_result != True:
-            print("[ID:%03d] groupSyncWrite addparam failed" % dxlIDs[motorIndex])
-        #print("groupSyncWrite for [ID:%03d] works" % (DXL_ID[device_index]))
-   
-    #Syncwrite goal position to bicep and forearm motors
-    dxl_comm_result = motor_sync_write.txPacket()
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        print("Motors are simultaneously rotating. DXL ID: ", dxlIDs)
+        # ------------------Start to execute motor rotation------------------------
+        while 1:
+            #Convert angle inputs into step units for movement
+            for id in range(idNum):
+                dxl_goal_inputs[id] = _map(dxl_goal_angle[id], 0, 360, 0, 4095)
+            print("Goal angles are ", dxl_goal_angle)
 
-    #Clear syncwrite parameter storage
-    motor_sync_write.clearParam()
+            simWrite(dxl_goal_inputs, dxlIDs)
+            dxl_end_position, movementStatus = simPosCheck(dxl_goal_inputs, dxlIDs)
+            for id in range(idNum):
+                dxl_end_angle[id] = _map(dxl_end_position[id], 0, 4095, 0, 360)
+            
+            for id in range(idNum):
+                print("Angle for Dynamixel:%03d is %03d ----------------------------" % (dxlIDs[id], dxl_end_angle[id]))
+            # ------------------------------------------------------------------------------------------------------------------------------------------------------
+            print("-------------------------------------")
+            return movementStatus
+    else:
+        print("ERROR: Number of angle inputs not matching with number of DXL ID inputs")
+        return movementStatus
 
-def Sim2MotorsRead(dxlIDs, pos_data_address, len_data_adress):
-        dxl_present_position = [0] * len(dxlIDs)
-        # Syncread present position
-        dxl_comm_result = motor_sync_read.txRxPacket()
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-
-        # Check if groupsyncread data of Dynamixel Bicep is available
-        for motorID in dxlIDs:
-            dxl_getdata_result = motor_sync_read.isAvailable(motorID, pos_data_address, len_data_adress)
-            if dxl_getdata_result != True:
-                print("[ID:%03d] groupSyncRead getdata failed" % motorID)
-
-        # Get Dynamixel#1 present position value
-        for motorIndex in dxlIDs:
-            dxl_present_position[motorIndex] = motor_sync_read.getData(dxlIDs[motorIndex], pos_data_address, len_data_adress)
-            #print("ID:%03d, position = %03d" % (dxlIDs[motorIndex], dxl_present_position[motorIndex]))
-
-        return dxl_present_position[motorIndex] 
 
 def portTermination():
     TORQUE_DISABLE = 0
@@ -278,9 +239,9 @@ def _map(x, in_min, in_max, out_min, out_max):
 #Reads the data of one motor in a given address. Dynamixel XM430-W350-R has most data in 4 bytes
 #Input is (ID of motor to be read, address where data resides)
 #Output is the data value that was read
-def ReadMotorData(motorIndex, data_address):
+def ReadMotorData(motorID, data_address):
     data_value, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(
-        portHandler, DXL_ID[motorIndex], data_address)
+        portHandler, motorID, data_address)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
@@ -290,9 +251,9 @@ def ReadMotorData(motorIndex, data_address):
 #Writes data to one motor at a given address. Dynamixel XM430-W350-R has most data in 4 bytes
 #Input is (ID of motor to be read, address where data resides, data you want to write)
 #There is no output
-def WriteMotorData(motorIndex, data_address, data_inputs):
+def WriteMotorData(motorID, data_address, data_inputs):
     dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(
-    portHandler, DXL_ID[motorIndex], data_address, data_inputs)
+    portHandler, motorID, data_address, data_inputs)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
@@ -336,6 +297,99 @@ def motor_check(motorIndex, goal_position):
 
     return (motor_present_position, motor_status)
 
+#The functions take in an array of X angle inputs and an array of X dynamixel IDs. Running the functions will drive the X dynamixels to the desired angle inputs simultaneously
+def simWrite(dxl_goal_inputs, dxlIDs):
+    idNum = len(dxlIDs)
+    #Intializate simultaneous motor movement
+    global motor_sync_write
+    motor_sync_write = GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_POSITION, LEN_GOAL_POSITION)
+    global motor_sync_read
+    motor_sync_read = GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+    #print("Motor_Sync_Write is ", motor_sync_write)
+    #print("Motor_Sync_Read is ", motor_sync_read)
+
+    #Create parameter storage for present positions
+    for id in range(idNum):
+        dxl_addparam_result = motor_sync_read.addParam(dxlIDs[id])
+        if dxl_addparam_result != True:
+            print("[ID:%03d] groupSyncRead addparam failed" % dxlIDs[id])
+        #print("DXL_ADDPARAM_RESULT is " ,dxl_addparam_result)
+
+    param_goal_position = [0] * idNum
+    #Allocate goal position values into 4-byte array for bicep and forearm motors. Dynamixel motors use either 2-bytes or 4-bytes
+    for id in range(idNum): 
+        param_goal_position[id] = [DXL_LOBYTE(DXL_LOWORD(dxl_goal_inputs[id])), DXL_HIBYTE(DXL_LOWORD(dxl_goal_inputs[id])),DXL_LOBYTE(DXL_HIWORD(dxl_goal_inputs[id])), DXL_HIBYTE(DXL_HIWORD(dxl_goal_inputs[id]))]
+    #print("param_goal_bicep_position is ", param_goal_position[1])
+    #print("param_goal_bicep_position is ", param_goal_position[2])
+
+    for id in range(idNum): 
+    #Add goal position input values of bicep and forearm motors to Syncwrite parameter storage
+        dxl_addparam_result = motor_sync_write.addParam(dxlIDs[id], param_goal_position[id])
+        if dxl_addparam_result != True:
+            print("[ID:%03d] groupSyncWrite addparam failed" % dxlIDs[id])
+        #print("groupSyncWrite for [ID:%03d] works" % (DXL_ID[device_index]))
+   
+    #Syncwrite goal position to bicep and forearm motors
+    dxl_comm_result = motor_sync_write.txPacket()
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+
+    #Clear syncwrite parameter storage
+    motor_sync_write.clearParam()
+
+def simPosCheck(dxl_goal_inputs, dxlIDs):
+    idNum = len(dxlIDs)
+    def simReadData():
+        dxl_present_position = [0] * idNum
+        # Syncread present position
+        dxl_comm_result = motor_sync_read.txRxPacket()
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+
+        # Check if groupsyncread data of Dynamixel is available
+        for motorID in dxlIDs:
+            dxl_getdata_result = motor_sync_read.isAvailable(motorID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+            if dxl_getdata_result != True:
+                print("[ID:%03d] groupSyncRead getdata failed" % motorID)
+
+        # Get Dynamixel present position value
+        for id in range(idNum):
+            dxl_present_position[id] = motor_sync_read.getData(dxlIDs[id], ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+            #print("ID:%03d, position = %03d" % (dxlIDs[motorIndex], dxl_present_position[motorIndex]))
+
+        return dxl_present_position
+    
+    print("Simultaneous position checking. DXL IDs being read: ", dxlIDs)
+    repetition_status = [0] * idNum
+    movement_status = [0] * idNum
+
+    present_position = simReadData()
+    kicker = 0
+    while (kicker == 0):
+        new_position = simReadData()
+        #Kicker method to prevent the infinite looping of the motor
+        for id in range(idNum):
+            if (abs(new_position[id] - present_position[id]) < 2) and movement_status[id] == 0:
+                repetition_status[id] += 1
+            else:
+                repetition_status[id] = 0
+            if repetition_status[id] >= 10:
+                kicker = 1
+        
+        present_position = new_position
+
+        movement_complete_count = 0
+        for id in range(idNum):
+            if (abs(dxl_goal_inputs[id]- present_position[id]) < DXL_MOVING_STATUS_THRESHOLD):
+                movement_complete_count += 1
+                movement_status[id] = 1
+        
+        if (movement_complete_count == idNum):
+            kicker = 1
+    
+    return present_position, movement_status
+
+    
 ##Old Test Cases
 #portInitialization(portname, baudrate, baseID, bicepID, forearmID):
 """portInitialization('COM3', 1000000, 1, 2, 3, 4, 5)
